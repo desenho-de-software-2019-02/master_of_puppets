8be5f9ec0835c344dde3e4616b66f435db7c0534
Andre Bargas <andrebargas@gmail.com>
2019-09-23 15:01:05 -0300

    dojo for basic structure
    Co-authored-by: arthur<arthurarp97@gmail.com>

--- services/resources/app.py
+++ services/resources/app.py
@@ -1,14 +1,14 @@
 from flask import Flask
 
 from flask_restplus import Api
-from s_apis import test
+from views import character_api
 
 app = Flask(__name__)
 api = Api(app)
 
 if __name__ == '__main__':
-    api.add_namespace(test.api)
-    app.run(host='0.0.0.0', port=5000)
+    api.add_namespace(character_api.api)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 
 
