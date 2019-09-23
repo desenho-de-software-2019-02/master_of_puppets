f3187da03ef27db98ed43f7dec0d22e12e9e2a22
Andre Bargas <ahbargas@latam.stefanini.com>
2019-09-23 14:13:34 -0300

    commit inicial

---
+++ services/auth/s_apis/test.py
@@ -0,0 +1,13 @@
+from flask_restplus import Namespace, Resource
+
+api = Namespace('test', description='teste teste')
+
+
+@api.route('')
+class Test(Resource):
+    @api.doc('teste')
+    def get(self):
+        return "teste"
+
+    def post(self):
+        return "test_post"
