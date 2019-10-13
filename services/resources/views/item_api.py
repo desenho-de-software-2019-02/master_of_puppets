6214b82e4f460a003838ba3978e59825da47632b
Mateus Nóbrega <mateusn@outlook.com>
2019-10-13 16:08:10 -0300

    Adding API documentation

--- services/resources/views/item_api.py
+++ services/resources/views/item_api.py
@@ -1,11 +1,18 @@
 import json
-from flask_restplus import Namespace, Resource
+from flask_restplus import Namespace, Resource, fields
 from flask import request, jsonify
 from mongoengine import DoesNotExist, ValidationError
 
 from controller.item_controller import ItemController
 
 api = Namespace('items', description='Item namespace')
+
+item_model = api.model('Item', {
+    'name': fields.String(required=True, description='Item name'),
+    'description': fields.String(required=True, description='Item description'),
+    'price': fields.Float(required=True, description='Item price'),
+    'weight': fields.Float(required=True, description='Item weight')
+})
 
 
 @api.route('/')
@@ -18,6 +25,8 @@ class ItemList(Resource):
         return jsonify(query)
 
     @api.doc("Item creation")
+    @api.expect(item_model)
+    # @api.marshal_with(item_model)
     def post(self):
         controller = ItemController(request)
         args = controller.new()
@@ -26,10 +35,14 @@ class ItemList(Resource):
 
 
 @api.route('/<string:id>')
+@api.response(200, 'Success')
+@api.response(400, 'Item not found')
+@api.param('id', 'Item identifier')
 class ItemDetail(Resource):
     param = "An integer that represents the item's id"
 
     @api.doc("Get information of a specific item", params={'id': param})
+    @api.response(400, 'Item not found')
     def get(self, id):
         controller = ItemController(request)
 
@@ -41,6 +54,7 @@ class ItemDetail(Resource):
         return json.loads(item)
 
     @api.doc("Update an item", params={'id': param})
+    @api.expect(item_model)
     def put(self, id):
         controller = ItemController(request)
 
