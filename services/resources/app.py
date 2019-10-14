9de5e3423dda178935f94677b4e7841abe55002c
Andre Filho <andre.filho001@outlook.com>
2019-10-14 09:22:46 -0300

    Makes docker image skinny again

--- services/resources/app.py
+++ services/resources/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 from flask_restplus import Api
-from views import character_api, item_api
+from views import character_api, item_api, dice_api
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,8 +9,5 @@ api = Api(app)
 if __name__ == '__main__':
     api.add_namespace(character_api.api)
     api.add_namespace(item_api.api)
+    api.add_namespace(dice_api.api)
     app.run(host='0.0.0.0', port=5000, debug=True)
-
-
-
-
--- services/resources/run.sh
+++ services/resources/run.sh
@@ -1,1 +1,7 @@
-python3 app.py
+#! /bin/bash
+
+echo '======= CHECKING FOR UNINSTALLED PKGs AND INSTALLING'
+pip freeze || pip install -r requirements.txt
+
+echo '======= RUNNING SERVER'
+python app.py
