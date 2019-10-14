89db5a0f31eba529a8f7575abf393bbab33c96be
Yoshida-Eduardo <edry97@hotmail.com>
2019-10-14 11:40:37 -0300

    Updates matches models

--- services/matches/models/match.py
+++ services/matches/models/match.py
@@ -1,16 +1,15 @@
 from pymongo import MongoClient
+from flask_api import status
+# from bson import ObjectID
 
-client = MongoClient('localhost', 27017)
+
+client = MongoClient('mongo', 27017)
 db = client['mop']
 match_collection = db['mop_match']
 
-def validate_match3(data):
-    if data["players"] is not []:
-        return True
-    return False
+class matchModel:
 
-def create_new_match(data):
-    if validate_match(data):
+    def create_new_match(data):
         new_element = {
             "gameMaster": data["gameMaster"],
             "players": data["players"],
@@ -19,34 +18,34 @@ def create_new_match(data):
             "session": data["session"],
             "date_initial": data["session"]
         }   
-        match_collection.insert_one(new_element)
-        return "Sucessfully create"
-    return "Failed to create"
+        print(match_collection.insert_one(new_element))
+        return status.HTTP_201_CREATED
+        return status.HTTP_400_BAD_REQUEST
 
-def read_match():
-    matchs = []
-    all_matchs = match_collection.find() 
-    for match in all_matchs:
-        element = {
-            "gameMaster": match["gameMaster"],
-            "players": match["players"],
-            "characters": match["characters"],
-            "rules": match["rules"],
-            "session": match["session"],
-            "date_initial": match["session"]
-        }
-        matchs.append(info) 
-    
-    if matchs == []:
-        return "there are no matchs yet"
-    return matchs
+    def list_matches():
+        matchs = []
+        all_matchs = match_collection.find() 
+        for match in all_matchs:
+            element = {
+                "gameMaster": match["gameMaster"],
+                "players": match["players"],
+                "characters": match["characters"],
+                "rules": match["rules"],
+                "session": match["session"],
+                "date_initial": match["session"]
+            }
+            matchs.append(element)         
+        return matchs, status.HTTP_200_OK
+    #WIP
+    # def delete_match(id):
+    #     match_collection.delete_one({'_id': ObjectId(id)})
 
-def gameMaster_matchs(name):
-    all_matchs = match_collection.find()
-    match_gameMaster = []
-    for match in all_match:
-        if match['gameMaster'] == name:
-            match_gameMaster.append(match)
-    if match_gameMaster:
-        return "This master has no matches"
-    return match_gameMaster
+    def gameMaster_matchs(name):
+        all_matchs = match_collection.find()
+        match_gameMaster = []
+        for match in all_match:
+            if match['gameMaster'] == name:
+                match_gameMaster.append(match)
+        if match_gameMaster:
+            return "This master has no matches"
+        return match_gameMaster
