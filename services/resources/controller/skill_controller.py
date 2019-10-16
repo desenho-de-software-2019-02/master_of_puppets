fa5da0193231cbf2908fb9edf5c6aa309166e302
lmmLucasMachado <lmlucasmachadom@gmail.com>
2019-10-16 11:38:56 -0300

    Add methods skill

--- services/resources/controller/skill_controller.py
+++ services/resources/controller/skill_controller.py
@@ -24,3 +24,42 @@ class SkillController:
 
         return parse_result
 
+    @staticmethod
+    def list():
+        """
+        Makes a query to list all skills
+        """
+
+        list_of_skills = list(map(lambda skill: loads(skill.to_json()), skill.objects.all()))
+        return list_of_skills
+
+    def edit(self, identifier):
+        """
+        Edits an skill given its id
+        """
+        
+        skill = skill.objects.get(id=identifier)
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', required=True)
+        parser.add_argument('usage_type', required=True)
+        parser.add_argument('description', required=True)
+        parser.add_argument('depends_on_items', required=True)
+        parse_result = parser.parse_args(req=self.request)
+
+        no_docs_updated = skill.update(**parse_result)
+
+        if no_docs_updated == 1:  # the row was updated successfully
+            return loads(skill.to_json())
+
+    @staticmethod
+    def delete(identifier):
+        """
+        Deletes an skill given its id
+        """
+        target = skill.objects.get(id=identifier)
+        target_data = loads(target.to_json())
+
+        target.delete()
+
+        return target_data
