509ac0a1674a312a1c278d43f018cff9f364e5f3
lmmLucasMachado <lmlucasmachadom@gmail.com>
2019-10-16 22:22:47 -0300

    Create api for skill

--- services/resources/app.py
+++ services/resources/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 from flask_restplus import Api
-from views import character_api, item_api, dice_api
+from views import character_api, item_api, dice_api, skill_api
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,4 +10,5 @@ if __name__ == '__main__':
     api.add_namespace(character_api.api)
     api.add_namespace(item_api.api)
     api.add_namespace(dice_api.api)
+    api.add_namespace(skill_api.api)
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ services/resources/views/skill_api.py
@@ -0,0 +1,73 @@
+import json
+from flask_restplus import Namespace, Resource, fields
+from flask import request, jsonify
+from mongoengine import DoesNotExist, ValidationError
+
+from controller.skill_controller import SkillController
+
+api = Namespace('skill', description='Skill namespace')
+
+skill_model = api.model('Skill', {
+    'name': fields.String(required=True, description='Skill name'),
+    'usage_type': fields.String(required=True, description='Usage type'),
+    'description': fields.String(required=True, description='Skill description'),
+    'depends_on_skills': fields.ListField(required=True, description='Item price'),
+})
+
+
+@api.route('/')
+class SkillList(Resource):
+    @api.doc("Item List")
+    def get(self):
+        controller = SkillController(request)
+        query = controller.list()
+
+        return jsonify(query)
+
+    @api.doc("Skill creation")
+    @api.expect(Skill_model)
+
+    def post(self):
+        controller = SkillController(request)
+        args = controller.new()
+
+        return args
+
+
+@api.route('/<string:id>')
+@api.response(200, 'Success')
+@api.response(400, 'Skill not found')
+@api.param('id', 'Skill identifier')
+class SkillDetail(Resource):
+    param = "An integer that represents the skill's id"
+
+    @api.doc("Get information of a specific skill", params={'id': param})
+    @api.response(400, 'Skill not found')
+    def get(self, id):
+        controller = SkillController(request)
+
+        try:
+            skill = controller.get_element_detail(id)
+        except (DoesNotExist, ValidationError):
+            api.abort(400, "Skill with id {} does not exist".format(id))
+
+        return json.loads(skill)
+
+    @api.doc("Update an skill", params={'id': param})
+    @api.expect(skill_model)
+    def put(self, id):
+        controller = SkillController(request)
+
+        try:
+            new_skill = controller.edit(id)
+        except (DoesNotExist, ValidationError):
+            api.abort(400, "Skill with id {} does not exist".format(id))
+
+        return new_skill
+
+    @api.doc("Delete an skill", params={'id': param})
+    def delete(self, id):
+        controller = SkillController(request)
+        deleted = controller.delete(id)
+
+        return deleted
