228d659a09d38b57eb95def096d4dacdc56f3f9b
Yoshida-Eduardo <edry97@hotmail.com>
2019-10-20 19:56:28 -0300

    Updates campaign controller

--- services/campaigns/controller/campaign_controller.py
+++ services/campaigns/controller/campaign_controller.py
@@ -1,21 +1,56 @@
-from models.campaign import campaignModel
+from json import dumps, loads
+from models.campaign import Campaign
 
-class campaignController:
-    def create_campaign(data):
+from flask_restplus import reqparse
 
-        # Validar os dados do character
-        return campaignModel.create_new_campaign(data) 
 
-    def get_campaigns():
-        return campaignModel.list_campaigns()
+class CampaignController:
+    def __init__(self, request):
+        self.request = request
 
-    def get_campaign_master(gameMaster):
-        list_campaign = gameMaster_campaigns(gameMaster)
-        if list_campaign:
-            return list_campaign
-        return 'This master no have campaign'
+    def new(self):
 
-    def delete_campaign(data):
-        id = data['_id']
-        return campaignModel.delete_campaign(id)
+        parser = reqpaser.RequestParser()
+        parser.add_argument('name', required=True)
+        parser.add_argument('gameMaster', required=True)
+        parser.add_argument('players')
+        parser.add_argument('characters')
+        parser.add_argument('rules')
+        parser.add_argument('session')
+        parse_result = parser.parse_args(req=self.request)
 
+        Campaign.from_json(dumps(parse_result)).save()
+    
+    @staticmethod
+    def list():
+        list_of_campaigns = list(map(lambda campaign: loads(campaign.to_json()), Campaign.objects.all())
+        return list_of_campaigns
+
+    @staticmethod
+    def get_element_detail(identifier):
+        return Campaign.objects.get(id=identifier).to_json()
+    
+    @staticmethod
+    def edit(self, identifier):
+        campaign = Campaign.objects.get(id=identifier)
+        parser = reqpaser.RequestParser()
+        parser.add_argument('name', required=True)
+        parser.add_argument('gameMaster', required=True)
+        parser.add_argument('players')
+        parser.add_argument('characters')
+        parser.add_argument('rules')
+        parser.add_argument('session')
+        parse_result = parser.parse_args(req=self.request)
+
+        no_docs_updated = campaign.update(**parse_result)
+
+        if no_docs_updated == 1:
+            returns loads(campaign.to_json())
+    
+    @staticmethod
+    def delete(identifier):
+        target = Campaign.objects.get(id=identififer)
+        target_data = loads(target.to_json())
+        target.delete()
+
+        return target_data
