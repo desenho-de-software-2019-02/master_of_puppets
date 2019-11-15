0c06d9cfbb409962d4e06246d5f458f78e9a42dc
Yoshida-Eduardo <edry97@hotmail.com>
2019-11-15 15:17:57 -0200

    Adds log_attack function with placeholder
    Co-authored: Lucas Machado <lmlucasmachadom@gmail.com>

--- services/campaigns/controller/damage_controller.py
+++ services/campaigns/controller/damage_controller.py
@@ -20,13 +20,19 @@ class DamageController:
         elif(self.request.get('item') is not None):
             self._strategy = ItemStrategy(self.request)
         else:
-            self._strategy = BasicAttackStrategy()
+            self._strategy = BasicAttackStrategy(self.request)
 
     def calculate(self):
         if(self.request.get('dice_result') == 20):
             return { 'critical_strike': True, 'succeeded': True }
         else:
-            return self._strategy.trial()
+            result = self._strategy.trial()
+            self._strategy.log_attack()
+            return result
+    
+    #Modify bellow method once merged with log system
+                    #    The attack {f'suceeded' if result.succeeded is True else 'failed'}
+                    #    {f'It was a critical strike' if result.critical_strike is True}")
 
 
 class DamageAction:
@@ -46,17 +52,27 @@ class DamageAction:
 
     def validate_threshold(self, threshold):
         if(self.request.get('dice_result') >= threshold):
-            return { 'critical_strike': False, 'succeeded': True}
+            self.result = { 'critical_strike': False, 'succeeded': True}
+            return self.result
         else:
-            return { 'critical_strike': False, 'succeeded': False}
+            self.result = {'critical_strike': False, 'succeeded': False}
+            return self.result
+    
+    def log_attack(self):
+        message = (
+           f"Player {self.caster.get('name')} attacked Player {self.target.get('name')} " 
+           f"with {self.attack_component['name'] if hasattr(self,'attack_component') else 'with a basic attack'}."
+           f"{'It was a critical strike' if self.result['critical_strike'] else ''}"
+        )
+        logging.warning(message)
 
 
 class SkillStrategy(DamageAction):
     def trial(self):
-        self.skill = self._get_skill()
-        threshold = self.caster.get(self.skill.get('attack_multiplier')) -\
-                    self.target.get(self.skill.get('defense_multiplier')) +\
-                    self.skill.get('attack_bonus')
+        self.attack_component = self._get_skill()
+        threshold = self.caster.get(self.attack_component.get('attack_multiplier')) -\
+                    self.target.get(self.attack_component.get('defense_multiplier')) +\
+                    self.attack_component.get('attack_bonus')
         return self.validate_threshold(threshold)
     
     def _get_skill(self):
@@ -66,15 +82,17 @@ class SkillStrategy(DamageAction):
 
 class BasicAttackStrategy(DamageAction):
     def trial(self):
-        logging.warning("Basic Attack Strategy")
+        threshold = self.caster.get('strength') -\
+                    self.target.get('armor_class')
+        return self.validate_threshold(threshold)
+
 
 class ItemStrategy(DamageAction):
     def trial(self):
-        self.item = self._get_item()
+        self.attack_component = self._get_item()
         threshold = self.caster.get('strength') -\
-                    self.target.get('costitution') +\
-                    self.item.get('proficiency')
-        logging.warning(threshold)
+                    self.target.get('armor_class') +\
+                    self.attack_component.get('proficiency')
         return self.validate_threshold(threshold)
 
     def _get_item(self):
