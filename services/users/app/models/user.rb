92498be80ebffd4b61a6b3d0fb82816a1e53f09f
Andre Filho <andre.filho001@outlook.com>
2019-11-16 14:17:13 -0300

    added devise-jwt
    Co-authored-by: machado <lmlucasmachadom@gmail.com>
    Co-authored-by: arturzao <arthurarp97@gmail.com>

--- services/users/app/models/user.rb
+++ services/users/app/models/user.rb
@@ -1,10 +1,5 @@
-class User
-  include Mongoid::Document
-  field :username, type: String
-  field :email, type: String
-  field :password, type: Digest
-
+class User < ApplicationRecord
   devise :database_authenticatable,
-          :jwt_authenticatable,
-          jwt_revocation_strategy: JWTBlacklist
+         :jwt_authenticatable,
+         jwt_revocation_strategy: JWTBlacklist
 end
--- services/users/start-run.sh
+++ services/users/start-run.sh
@@ -1,3 +1,28 @@
+bundle check || bundle update
+
+gem install rails
+
+while ! pg_isready -h users-db -p 5432 -q -U postgres; do
+	>&2 echo "postgres is unavailable"
+	sleep 1
+done
+
+>&2 echo "postgres is up"
+
+if !(bundle exec rake db:migrate); then
+  >&2 echo "=========== DATABASE DOES NOT EXIST... yet"
+	bundle exec rake db:create
+	>&2 echo "=========== DATABASE CREATED"
+	bundle exec rake db:migrate
+	>&2 echo "=========== DATABASE MIGRATED"
+  bundle exec rake db:seed
+  >&2 echo "=========== DATABASE SEEDED"
+else
+  >&2 echo "=========== DATABASE EXIST.... yay"
+	bundle exec rake db:migrate
+	>&2 echo "=========== DATABASE MIGRATED (already exists)"
+fi
+
 if (cat ./tmp/pids/server.pid); then
   >&2 echo "SERVER PID EXISTS... REMOVING..."
   rm ./tmp/pids/server.pid
